00a4b5ba8483c2cfda07aee6f04a2eae9890c5a9
Tarek M. Sayed <tareqmsayed@gmail.com>
2020-10-12 21:23:18 +0200

    [fix]: use cv alias

--- 08-01-houghTransform.py
+++ 08-01-houghTransform.py
@@ -9,21 +9,21 @@ Email: atamish@gmail.com
 
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2
+import cv2 as cv
 # global variables
 imagesPath = "images/"
 # read image in gray scale
-img = cv2.imread(imagesPath + "shapes.png")
-cv2.imshow("orignal image", img)
+img = cv.imread(imagesPath + "shapes.png")
+cv.imshow("orignal image", img)
 # convert to gray
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 # extract edges
-edges = cv2.Canny(gray, 100, 200)
+edges = cv.Canny(gray, 100, 200)
 r, r_theta = (1, np.pi/180)
 # TODO: sort array according to theta column to get a pretty plot
-accum = cv2.HoughLines(edges, r, r_theta, threshold=0, min_theta =-np.pi/2, max_theta=np.pi/2).reshape((-1,2))
+accum = cv.HoughLines(edges, r, r_theta, threshold=0, min_theta =-np.pi/2, max_theta=np.pi/2).reshape((-1,2))
 # check diffirent thresholds 50, 100, 150, 200
-peaks = cv2.HoughLines(edges, r, r_theta, threshold=150, min_theta =-np.pi/2, max_theta=np.pi/2).reshape((-1,2))
+peaks = cv.HoughLines(edges, r, r_theta, threshold=150, min_theta =-np.pi/2, max_theta=np.pi/2).reshape((-1,2))
 plt.plot(accum[:,1], accum[:,0], linewidth=0.03)
 plt.plot(peaks[:,1], peaks[:,0], 'ro')
 plt.xlabel('theta')
@@ -41,10 +41,10 @@ for rho,theta in peaks:
     x2 = int(x0 - 1000*(-b))
     y2 = int(y0 - 1000*(a))
     # plot every line
-    cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+    cv.line(img,(x1,y1),(x2,y2),(0,0,255),2)
 # Uncomment the next line to save the output image
-#cv2.imwrite('houghlines.png',img)
-cv2.imshow("hough lines", img)
+#cv.imwrite('houghlines.png',img)
+cv.imshow("hough lines", img)
 # wait to press any key to close
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv.waitKey(0)
+cv.destroyAllWindows()
